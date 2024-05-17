9f6bdf97e314d6d4a6b4089ffb5adcea4bd05912
eunjinnnn <sally0680@naver.com>
2024-05-17 12:39:37 +0900

    add page: 버튼 위치 수정

--- frontend/src/routes/add/+page.svelte
+++ frontend/src/routes/add/+page.svelte
@@ -21,11 +21,11 @@
 </script>
 
 <form class = 'container mx-auto'>
+    <div class = 'flex flex-wrap -mx-2 p-2'>
+        <Label for="Category" class="mb-2 text-xs">Category</Label>
+        <Select id="select-category" style='font-size:x-small' size="sm" items={categories} placeholder="Category"/>
+    </div>
     <div class="flex flex-wrap -mx-2">
-        <div class = 'flex flex-col w-1/3 p-2'>
-            <Label for="Category" class="mb-2 text-xs">Category</Label>
-            <Select id="select-category" style='font-size:x-small' size="sm" items={categories} placeholder="Category"/>
-        </div>
         <div class = 'flex flex-col w-1/3 p-2'>
             <Label for="Foodname" class="mb-2 text-xs" >Foodname</Label>
             <Input type="text" class='rounded' id="foodname" style='font-size:x-small' placeholder="Foodname" required/>
@@ -34,22 +34,24 @@
             <Label for="Volume" class="mb-2 text-xs">Volume</Label>
             <Input type="number" class='rounded' id="volume" style='font-size:x-small' placeholder="Volume" required />
         </div>
-    </div>
-    <div class="flex flex-wrap -mx-2">
         <div class = 'flex flex-col w-1/3 p-2'>
             <Label for="Unit" class="mb-2 text-xs">Unit</Label>
             <Select id="select-category" class='' size="md" style='font-size:x-small' items={units} placeholder="Unit"/>
         </div>
-        <div class = 'flex flex-col w-1/3 p-2'>
+    </div>
+    <div class="flex flex-wrap -mx-2">
+        <div class = 'flex flex-col w-1/2 p-2'>
             <Label for="purchaseDate" class="mb-2 text-xs">Purchase Date</Label>
             <Input type="date" class=''  style='font-size:x-small' id="purchaseDate" required />
         </div>
-        <div class = 'flex flex-col w-1/3 p-2'>
+        <div class = 'flex flex-col w-1/2 p-2'>
             <Label for="expirationDate" class="mb-2 text-xs">Expiration date</Label>
             <Input type="date" class='' id="expirationDate" style='font-size:x-small' required />
         </div>
     </div>
-    <Button type="submit" size = 'xs' style='font-size:x-small'>ADD</Button>
+    <div class="flex justify-end">
+        <Button type="submit" class="text-xs" style="font-size: x-small;">ADD</Button>
+    </div>
 </form>
 
 <div class="container mx-auto">
@@ -64,5 +66,6 @@
     </div>
 </div>
 
-
-<Button type='submit' size = 'xs'> OK </Button>
+<div class="flex justify-end">
+    <Button type='submit' size = 'xs'> OK </Button>
+</div>
