e4b56e5507e1ca8416820b4e9d500d10df80dd26
eunjinnnn <sally0680@naver.com>
2024-05-17 12:12:19 +0900

    add page: 추가한 food 3개씩 밑에 뜨도록 변경하기

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   let nonActiveClass = 'text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
 </script>
 
-<Navbar let:NavContainer color="green" >
+<Navbar let:NavContainer color="green">
   <NavBrand href="/">
     <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white" style='font-size:small'>REFRIGERATOR</span>
   </NavBrand>
--- frontend/src/routes/add/+page.svelte
+++ frontend/src/routes/add/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Input, Label, Helper, Button, Checkbox, A , Select, Card} from 'flowbite-svelte';
 
-  let cat_selected = ['food1', 'food2'];
+  let cat_selected = ['food1', 'food2', 'food3', 'food4', 'food5'];
   let unit_selected;
   let volume;
   let foodname;
@@ -20,42 +20,49 @@
     ];
 </script>
 
-<form class = 'flex-1 w-20'>
-    <div class="flex gap-6 mb-6">
-        <div class = 'fiex w-30 h-16'>
-            <Label for="purchaseDate" class="mb-2" style='font-size:x-small'>Category</Label>
-            <Select id="select-category" class='fiex w-20' style='font-size:x-small' size="sm" items={categories} placeholder="Category"/>
+<form class = 'flex-1 w-25'>
+    <div class="flex gap-2 mb-2">
+        <div class = 'flex flex-col w-30 h-16'>
+            <Label for="purchaseDate" class="mb-2 text-xs" >Category</Label>
+            <Select id="select-category" class='flex w-20' style='font-size:x-small' size="sm" items={categories} placeholder="Category"/>
         </div>
-        <div class = 'fiex w-30 h-16'>
-            <Label for="purchaseDate" class="mb-2" style='font-size:x-small'>Foodname</Label>
-            <Input type="text" class='fiex w-20' id="foodname" style='font-size:x-small' placeholder="Foodname" required/>
+        <div class = 'flex flex-col w-30 h-16'>
+            <Label for="Foodname" class="mb-2 text-xs" >Foodname</Label>
+            <Input type="text" class='flex w-20 rounded-md' id="foodname" style='font-size:x-small' placeholder="Foodname" required/>
         </div>
-        <div class = 'fiex w-30 h-16'>
-            <Label for="purchaseDate" class="mb-2" style='font-size:x-small'>Volume</Label>
-            <Input type="number" class='fiex w-20' id="volume" style='font-size:x-small' placeholder="Volume" required />
+        <div class = 'flex flex-col w-30 h-16'>
+            <Label for="Volume" class="mb-2 text-xs">Volume</Label>
+            <Input type="number" class='flex w-20 ' id="volume" style='font-size:x-small' placeholder="Volume" required />
         </div>
     </div>
-    <div class="flex gap-6 mb-6">
-        <div class = 'fiex w-30 h-16'>
-            <Label for="purchaseDate" class="mb-2" style='font-size:x-small'>Unit</Label>
-            <Select id="select-category" class='fiex w-20' size="md" style='font-size:x-small' items={units} placeholder="Unit"/>
+    <div class="flex gap-2 mb-6">
+        <div class = 'flex flex-col w-30 h-16'>
+            <Label for="Unit" class="mb-2 text-xs">Unit</Label>
+            <Select id="select-category" class='flex w-20' size="md" style='font-size:x-small' items={units} placeholder="Unit"/>
         </div>
-        <div class = 'fiex w-30 h-16'>
-            <Label for="purchaseDate" class="mb-2" style='font-size:x-small'>Purchase Date</Label>
-            <Input type="date" class='fiex w-20'  style='font-size:x-small' id="purchaseDate" required />
+        <div class = 'flex flex-col w-30 h-16'>
+            <Label for="purchaseDate" class="mb-2 text-xs">Purchase Date</Label>
+            <Input type="date" class='flex w-20'  style='font-size:x-small' id="purchaseDate" required />
         </div>
-        <div class = 'fiex w-30 h-16'>
-            <Label for="expirationDate" class="mb-2" style='font-size:x-small'>Expiration date</Label>
-            <Input type="date" class='fiex w-20' id="expirationDate" style='font-size:x-small' required />
+        <div class = 'flex flex-col w-30 h-16'>
+            <Label for="expirationDate" class="mb-2 text-xs">Expiration date</Label>
+            <Input type="date" class='flex w-20' id="expirationDate" style='font-size:x-small' required />
         </div>
     </div>
     <Button type="submit" size = 'xs' style='font-size:x-small'>ADD</Button>
 </form>
 
-{#each cat_selected as cat}
-    <Card class = 'flex-1 w-20'>
-        <p>{cat}</p>
-    </Card>
-{/each}
+<div class="container mx-auto">
+    <div class="flex flex-wrap -mx-2">
+        {#each cat_selected as cat}
+            <div class="w-1/3 p-2">
+                <Card class="w-full">
+                    <div class="flex-wrap text-xs">{cat}</div>
+                </Card>
+            </div>
+        {/each}
+    </div>
+</div>
+
 
 <Button type='submit' size = 'xs'> OK </Button>
