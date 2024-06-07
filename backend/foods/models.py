3cffaa7e620d382383a8d52f23f96b9f6a3943be
eunjinnnn <sally0680@naver.com>
2024-06-07 22:11:27 +0900

    make migrations

--- backend/foods/models.py
+++ backend/foods/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    img_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class FoodItem(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    foodname = models.CharField(max_length=255)
+    volume = models.IntegerField()
+    unit = models.CharField(max_length=50)
+    expiration_date = models.DateField()
+    purchase_date = models.DateField()
+
+    def __str__(self):
+        return self.foodname
