abff0670db426add9fa52cd05d277408d2bb5d4d
eunjinnnn <sally0680@naver.com>
2024-06-07 22:59:47 +0900

    model 수정 - category, foodunit, fooditem table, 초기 데이터 정의

--- backend/foods/models.py
+++ backend/foods/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -9,13 +9,41 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class FoodUnit(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class FoodItem(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     foodname = models.CharField(max_length=255)
     volume = models.IntegerField()
-    unit = models.CharField(max_length=50)
+    unit = models.ForeignKey(FoodUnit, on_delete=models.CASCADE)
     expiration_date = models.DateField()
-    purchase_date = models.DateField()
+    purchase_date = models.DateField(default=timezone.now)  # Default 값으로 현재 날짜 사용
+
+    def clean(self):
+        if self.expiration_date < self.purchase_date:
+            raise ValidationError("Expiration date must be after purchase date.")
 
     def __str__(self):
         return self.foodname
+
+# # 초기 데이터 정의
+# initial_units = [
+#     FoodUnit(name='개'),
+#     FoodUnit(name='조각'),
+#     FoodUnit(name='L'),
+# ]
+
+# initial_categories = [
+#     Category(name='VEGETABLES'),
+#     Category(name='DRINKS'),
+#     Category(name='FROZEN_FOODS'),
+#     Category(name='ETC'),
+# ]
+
+# # 초기 데이터를 한 번에 생성
+# FoodUnit.objects.bulk_create(initial_units)
+# Category.objects.bulk_create(initial_categories)
