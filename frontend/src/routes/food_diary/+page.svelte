adafe96c347de98bc36eb978d5bd251d9ee97847
eunjinnnn <sally0680@naver.com>
2024-06-12 12:52:10 +0900

    간단히 food diary 추가하기

--- frontend/src/routes/food_diary/+page.svelte
+++ frontend/src/routes/food_diary/+page.svelte
@@ -7,32 +7,28 @@
 <div class="bg-zinc-50/70 flex flex-col mb-5 overflow-hidden border rounded-xl shadow-md p-4 space-y-4">
   <div class="bg-white p-4 rounded-lg shadow-md flex flex-col">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">닭가슴살 샐러드 파스타</h5>
-      <div class="flex flex-row">
+      <div class="flex flex-col">
           <img src="/images/image-1.webp" alt="Image" class="rounded-lg w-1/3 h-64 object-cover" />
-          <div class="flex flex-col justify-between p-4 w-2/3">
-              <p class="flex mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-              </p>
-              <Button class='flex border-[1px] border-zinc-500 text-xxs font-semibold font-PoetsenOne  bg-[#E8C9D5]/50 text-zinc-950 hover:bg-[#E8C9D5]'>
-                  Read more
-              </Button>
-          </div>
+          <p class="flex justify-center mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+            <ui># 다이어트식</ui> <ui># 간단요리</ui>
+          </p>
+          <Button class='flex border-[1px] border-zinc-500 text-xxs font-semibold font-PoetsenOne  bg-[#E8C9D5]/50 text-zinc-950 hover:bg-[#E8C9D5]'>
+            Read more
+          </Button>
       </div>
   </div>
 
   <div class="bg-white p-4 rounded-lg shadow-md flex flex-col">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">닭가슴살 샐러드 파스타</h5>
-      <div class="flex flex-row">
-          <img src="/images/image-1.webp" alt="Image" class="rounded-lg w-1/3 h-64 object-cover" />
-          <div class="flex flex-col justify-between p-4 w-2/3">
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-              </p>
-              <Button class = 'flex border-[1px] border-zinc-500 text-xxs font-semibold font-PoetsenOne  bg-[#E8C9D5]/50 text-zinc-950 hover:bg-[#E8C9D5]'>
-                  Read more
-              </Button>
-          </div>
-          
-      </div>
-  </div>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">양배추 참치 덮밥</h5>
+    <div class="flex flex-col">
+        <img src="/images/image-1.webp" alt="Image" class="rounded-lg w-1/3 h-64 object-cover" />
+        <p class="flex justify-center mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          <ui># 다이어트식</ui> <ui># 간단요리</ui> <ui># 한식</ui>
+        </p>
+        <Button class='flex border-[1px] border-zinc-500 text-xxs font-semibold font-PoetsenOne  bg-[#E8C9D5]/50 text-zinc-950 hover:bg-[#E8C9D5]'>
+          Read more
+        </Button>
+    </div>
+</div>
+
 </div>
